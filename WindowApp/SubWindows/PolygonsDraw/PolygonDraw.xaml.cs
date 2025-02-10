8c8fc2ee540e1cc6688d82157e32fccfffc407d4
mrchysen <mrchysen@mail.ru>
2025-02-10 07:51:13 +0700

    p-15: save 1

--- WindowApp/SubWindows/PolygonsDraw/PolygonDraw.xaml.cs
+++ WindowApp/SubWindows/PolygonsDraw/PolygonDraw.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using Core.Models.Points;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
@@ -14,6 +15,8 @@ public partial class PolygonDraw : Window
     public const int DeltaX = 20;
     public const int GridYLines = 59;
     public const int DeltaY = 20;
+    public const double DeltaArrow = 5;
+    public const double ArrowHeight = 22;
 
     public PolygonDrawContext PolygonDrawContext => _context;
 
@@ -43,6 +46,40 @@ public partial class PolygonDraw : Window
         line.SetValue(RenderOptions.EdgeModeProperty, EdgeMode.Aliased);
 
         return line;
+    }
+
+    public static (Line, Line) CreateArrow(Point p1, Point p2, Brush? brush = null)
+    {
+        var line1 = new Line();
+        var line2 = new Line();
+
+        PointD normal = new PointD(- p2.Y + p1.Y, p2.X - p1.X);
+        normal = normal * (1.0 / normal.Norm());
+
+        var vec = new PointD(p2.X - p1.X, p2.Y - p1.Y);
+        vec = vec * (1.0 / vec.Norm());
+
+        line1.X1 = p2.X;
+        line1.Y1 = p2.Y;
+        line1.X2 = p2.X - ArrowHeight * vec.X + normal.X * DeltaArrow;
+        line1.Y2 = p2.Y - ArrowHeight * vec.Y + normal.Y * DeltaArrow;
+
+        line2.X1 = p2.X;
+        line2.Y1 = p2.Y;
+        line2.X2 = p2.X - ArrowHeight * vec.X - normal.X * DeltaArrow;
+        line2.Y2 = p2.Y - ArrowHeight * vec.Y - normal.Y * DeltaArrow;
+
+        line1.Stroke = brush ?? Brushes.Black;
+        line1.StrokeThickness = 2;
+        line1.SnapsToDevicePixels = true;
+        line1.SetValue(RenderOptions.EdgeModeProperty, EdgeMode.Aliased);
+
+        line2.Stroke = brush ?? Brushes.Black;
+        line2.StrokeThickness = 4;
+        line2.SnapsToDevicePixels = true;
+        line2.SetValue(RenderOptions.EdgeModeProperty, EdgeMode.Aliased);
+
+        return (line1, line2);
     }
 
     private void DrawCanvasGrid()
@@ -88,9 +125,12 @@ public partial class PolygonDraw : Window
 
         if (_context.Points.Count > 0)
         {
-            var line = CreateLine(point, _context.Points.Last());
+            var line = CreateLine(_context.Points.Last(), point);
+            var arrowLines = CreateArrow(_context.Points.Last(), point);
 
             WindowCanvas.Children.Add(line);
+            WindowCanvas.Children.Add(arrowLines.Item1);
+            WindowCanvas.Children.Add(arrowLines.Item2);
 
             if (_context.Points.Count > 1)
             {
